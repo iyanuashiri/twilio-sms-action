9e90941c184ee220bfea59701f0aa495e01f6701
Iyanu Ajao <ajaoiyanu@gmail.com>
2019-02-16 19:24:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,6 @@ fi
 
 curl -X POST https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Messages.json \
 --data-urlencode "From=${TWILIO_CALLER_ID}" \
---data-urlencode "Body=${BODY}" \
+--data-urlencode "Body=$*" \
 --data-urlencode "To=${RECIPIENT_NUMBER}" \
 -u ${TWILIO_ACCOUNT_SID}:${TWILIO_AUTH_TOKEN}

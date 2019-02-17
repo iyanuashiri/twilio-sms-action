4bcefd6d588815bb9693d14bed962eb3ab0c0ae0
Iyanu Ajao <ajaoiyanu@gmail.com>
2019-02-17 18:13:07 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
+
 set -eu
-
-if [[ -z "$TWILIO_AUTH_TOKEN" ]]; then
-  echo "Set the TWILIO_AUTH_TOKEN secret."
-  exit 1
-fi
-
-if [[ -z "$TWILIO_ACCOUNT_SID" ]]; then
-  echo "Set the TWILIO_ACCOUNT_SID secret."
-  exit 1
-fi
-
-if [[ -z "$TWILIO_CALLER_ID" ]]; then
-  echo "Set the TWILIO_CALLER_ID secret."
-  exit 1
-fi
-
-if [[ -z "$RECIPIENT_NUMBER" ]]; then
-  echo "Set the RECIPIENT_NUMBER secret."
-  exit 1
-fi
-
 
 curl -X POST https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Messages.json \
 --data-urlencode "From=${TWILIO_CALLER_ID}" \
